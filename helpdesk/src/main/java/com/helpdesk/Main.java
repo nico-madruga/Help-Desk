92554d9136b47a3ac41b29967270a90d105c3fb7
nico-madruga <nizinhoms@gmail.com>
2026-08-30 16:06:03 -0300

    feat: Criada Main

---
+++ helpdesk/src/main/java/com/helpdesk/Main.java
@@ -0,0 +1,12 @@
+package com.helpdesk;
+
+import com.helpdesk.view.Menu;
+
+public class Main 
+{
+    static Menu menu = new Menu();
+    public static void main(String[] args)
+    {
+        menu.iniciar();
+    }
+}
