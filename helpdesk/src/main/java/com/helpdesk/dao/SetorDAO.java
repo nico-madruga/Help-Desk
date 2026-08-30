3ff5cdccfee33f3e2451e65aae26ff15cc8d6aa5
nico-madruga <nizinhoms@gmail.com>
2026-08-30 17:57:55 -0300

    feat: processo de cadastro parcialmente feito, esqueci da connectionfactory

--- helpdesk/src/main/java/com/helpdesk/dao/SetorDAO.java
+++ helpdesk/src/main/java/com/helpdesk/dao/SetorDAO.java
@@ -1,5 +1,18 @@
 package com.helpdesk.dao;
 
-public class SetorDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class SetorDAO 
+{
+    public void inserir()
+    {
+        String sql = """
+                INSERT INTO setores (nome)
+                VALUES (?)
+                """;
+
+        try()
+    }
 }
--- helpdesk/src/main/java/com/helpdesk/service/SetorService.java
+++ helpdesk/src/main/java/com/helpdesk/service/SetorService.java
@@ -12,5 +12,6 @@ public class SetorService
             throw new BusinessException("Nome é um campo obrigatório.");
         }
         
+        
     }
 }
