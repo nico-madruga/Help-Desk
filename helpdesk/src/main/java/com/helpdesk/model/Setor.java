8fbb062fd4dd4ea7dfd175ab3e00daae24aaca59
nico-madruga <nizinhoms@gmail.com>
2026-08-30 11:28:01 -0300

    feat/Criada model Setor

--- helpdesk/src/main/java/com/helpdesk/model/Setor.java
+++ helpdesk/src/main/java/com/helpdesk/model/Setor.java
@@ -1,5 +1,50 @@
 package com.helpdesk.model;
 
-public class Setor {
+public class Setor 
+{
+    private Integer setorId;
+    private String nome;
+
+    public Setor(int setorId, String nome)
+    {
+        this.setorId = setorId;
+        this.nome = nome;
+    }
+
+    public Setor()
+    {
+        this.setorId = 0;
+        this.nome = null;
+    }
+
+    public int getSetorId()
+    {
+        return setorId;
+    }
+
+    public void setSetorId(int setorId)
+    {
+        this.setorId = setorId;
+    }
+
+    public String getNome()
+    {
+        return nome;
+    }
+
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() 
+    {
+        return """
+                Setor
+                ID = %d
+                Nome = @s
+                """.formatted(setorId, nome);
+    }
 
 }
